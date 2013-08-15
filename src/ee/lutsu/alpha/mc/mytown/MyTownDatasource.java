b345a56cc545c60c620caa80214e35fb7491c10b
Johannes Frank <djgummikuh@gmail.com>
2013-08-15 08:47:54 +0200

    adding synchronization to MyTownDatatsource to prevent race conditions removing the range check for dimensions != 0 adding team chat

--- src/ee/lutsu/alpha/mc/mytown/MyTownDatasource.java
+++ src/ee/lutsu/alpha/mc/mytown/MyTownDatasource.java
@@ -1,254 +1,266 @@
 package ee.lutsu.alpha.mc.mytown;
 
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
-import ee.lutsu.alpha.mc.mytown.entities.Nation;
-import ee.lutsu.alpha.mc.mytown.entities.Resident;
-import ee.lutsu.alpha.mc.mytown.entities.Town;
-import ee.lutsu.alpha.mc.mytown.entities.TownBlock;
+import ee.lutsu.alpha.mc.mytown.entities.*;
 import ee.lutsu.alpha.mc.mytown.sql.MyTownDB;
 
 public class MyTownDatasource extends MyTownDB {
-    public static MyTownDatasource instance = new MyTownDatasource();
+	public static MyTownDatasource instance = new MyTownDatasource();
 
-    public HashSet<Resident> residents = new HashSet<Resident>();
-    public HashSet<Town> towns = new HashSet<Town>();
-    public HashSet<TownBlock> blocks = new HashSet<TownBlock>();
-    public HashSet<Nation> nations = new HashSet<Nation>();
+	public HashSet<Resident> residents = new HashSet<Resident>();
+	public HashSet<Town> towns = new HashSet<Town>();
+	public HashSet<TownBlock> blocks = new HashSet<TownBlock>();
+	public HashSet<Nation> nations = new HashSet<Nation>();
 
-    public void init() throws Exception {
-        residents = new HashSet<Resident>();
-        towns = new HashSet<Town>();
-        blocks = new HashSet<TownBlock>();
-        nations = new HashSet<Nation>();
+	public void init() throws Exception {
+		synchronized (residents) {
+			residents = new HashSet<Resident>();
+			towns = new HashSet<Town>();
+			blocks = new HashSet<TownBlock>();
+			nations = new HashSet<Nation>();
 
-        dispose();
-        connect();
-        load();
+			dispose();
+			connect();
+			load();
 
-        towns.addAll(loadTowns());
-        residents.addAll(loadResidents()); // links to towns
+			towns.addAll(loadTowns());
+			synchronized (residents) {
+				residents.addAll(loadResidents()); // links to towns
+			}
+			for (Town t : towns) {
+				for (TownBlock res : t.blocks()) {
+					if (res.owner_name != null) // map block owners
+					{
+						Resident r = getResident(res.owner_name);
+						res.sqlSetOwner(r);
+						res.owner_name = null;
+					}
 
-        for (Town t : towns) {
-            for (TownBlock res : t.blocks()) {
-                if (res.owner_name != null) // map block owners
-                {
-                    Resident r = getResident(res.owner_name);
-                    res.sqlSetOwner(r);
-                    res.owner_name = null;
-                }
+					blocks.add(res); // add block to global list
+				}
+			}
 
-                blocks.add(res); // add block to global list
-            }
-        }
+			nations.addAll(loadNations());
 
-        nations.addAll(loadNations());
+			addAllOnlinePlayers();
+		}
+	}
 
-        addAllOnlinePlayers();
-    }
+	public void addAllOnlinePlayers() {
+		for (Object obj : MinecraftServer.getServer().getConfigurationManager().playerEntityList) {
+			EntityPlayer pl = (EntityPlayer) obj;
+			getOrMakeResident(pl);
+		}
+	}
 
-    public void addAllOnlinePlayers() {
-        for (Object obj : MinecraftServer.getServer().getConfigurationManager().playerEntityList) {
-            EntityPlayer pl = (EntityPlayer) obj;
-            getOrMakeResident(pl);
-        }
-    }
+	public void addTown(Town t) {
+		towns.add(t);
+	}
 
-    public void addTown(Town t) {
-        towns.add(t);
-    }
+	public void addNation(Nation n) {
+		nations.add(n);
+	}
 
-    public void addNation(Nation n) {
-        nations.add(n);
-    }
-
-    public TownBlock getOrMakeBlock(int world_dimension, int x, int z) {
-        for (TownBlock res : blocks) {
+	public TownBlock getOrMakeBlock(int world_dimension, int x, int z) {
+		for (TownBlock res : blocks) {
             if (res.equals(world_dimension, x, z)) {
-                return res;
-            }
+				return res;
+		}
         }
 
-        TownBlock res = new TownBlock(world_dimension, x, z);
-        blocks.add(res);
-        return res;
-    }
+		TownBlock res = new TownBlock(world_dimension, x, z);
+		blocks.add(res);
+		return res;
+	}
 
-    public TownBlock getBlock(int world_dimension, int x, int z) {
-        for (TownBlock res : blocks) {
+	public TownBlock getBlock(int world_dimension, int x, int z) {
+		for (TownBlock res : blocks) {
             if (res.equals(world_dimension, x, z)) {
-                return res;
-            }
+				return res;
+		}
         }
 
-        return null;
-    }
+		return null;
+	}
 
-    public TownBlock getPermBlockAtCoord(int world_dimension, int x, int y,
-            int z) {
-        return getPermBlockAtCoord(world_dimension, x, y, y, z);
-    }
+	public TownBlock getPermBlockAtCoord(int world_dimension, int x, int y,
+			int z) {
+		return getPermBlockAtCoord(world_dimension, x, y, y, z);
+	}
 
-    public TownBlock getPermBlockAtCoord(int world_dimension, int x, int yFrom,
-            int yTo, int z) {
+	public TownBlock getPermBlockAtCoord(int world_dimension, int x, int yFrom,
+			int yTo, int z) {
         TownBlock targetBlock = getBlock(world_dimension, ChunkCoord
                 .getCoord(x), ChunkCoord.getCoord(z));
-        if (targetBlock != null && targetBlock.settings.yCheckOn) {
-            if (yTo < targetBlock.settings.yCheckFrom
+		if (targetBlock != null && targetBlock.settings.yCheckOn) {
+			if (yTo < targetBlock.settings.yCheckFrom
                     || yFrom > targetBlock.settings.yCheckTo) {
-                targetBlock = targetBlock
-                        .getFirstFullSidingClockwise(targetBlock.town());
-            }
+				targetBlock = targetBlock
+						.getFirstFullSidingClockwise(targetBlock.town());
+		}
         }
 
-        return targetBlock;
-    }
+		return targetBlock;
+	}
 
-    public Town getTown(String name) {
-        for (Town res : towns) {
+	public Town getTown(String name) {
+		for (Town res : towns) {
             if (res.name().equalsIgnoreCase(name)) {
-                return res;
-            }
+				return res;
+		}
         }
 
-        return null;
-    }
+		return null;
+	}
 
-    @Override
-    public Town getTown(int id) {
-        for (Town res : towns) {
+	@Override
+	public Town getTown(int id) {
+		for (Town res : towns) {
             if (res.id() == id) {
-                return res;
-            }
+				return res;
+		}
         }
 
-        return null;
-    }
+		return null;
+	}
 
-    public Nation getNation(String name) {
-        for (Nation res : nations) {
+	public Nation getNation(String name) {
+		for (Nation res : nations) {
             if (res.name().equalsIgnoreCase(name)) {
-                return res;
-            }
+				return res;
+		}
         }
 
-        return null;
-    }
+		return null;
+	}
 
-    public Resident getOrMakeResident(EntityPlayer player) {
-        for (Resident res : residents) {
+	public Resident getOrMakeResident(EntityPlayer player) {
+		synchronized (residents) {
+			for (Resident res : residents) {
             if (res.onlinePlayer == player) {
-                return res;
-            }
+					return res;
+			}
         }
 
-        Resident r = getOrMakeResident(player.getEntityName());
-        r.onlinePlayer = player;
-        return r;
-    }
+			Resident r = getOrMakeResident(player.getEntityName());
+			r.onlinePlayer = player;
+			return r;
+		}
+	}
 
-    public Resident getResident(EntityPlayer player) {
-        for (Resident res : residents) {
+	public Resident getResident(EntityPlayer player) {
+		synchronized (residents) {
+			for (Resident res : residents) {
             if (res.onlinePlayer == player) {
-                return res;
-            }
+					return res;
+			}
         }
 
-        return null;
-    }
+			return null;
+		}
+	}
 
-    public Resident getOrMakeResident(String name) // case sensitive
-    {
-        for (Resident res : residents) {
+	public Resident getOrMakeResident(String name) // case sensitive
+	{
+		synchronized (residents) {
+			for (Resident res : residents) {
             if (res.name().equals(name)) {
-                return res;
-            }
+					return res;
+			}
         }
 
-        Resident res = new Resident(name);
-        residents.add(res);
-        return res;
-    }
+			Resident res = new Resident(name);
+			residents.add(res);
+			return res;
+		}
+	}
 
-    public Resident getResident(String name) // case in-sensitive
-    {
-        for (Resident res : residents) {
+	public Resident getResident(String name) // case in-sensitive
+	{
+		synchronized (residents) {
+			for (Resident res : residents) {
             if (res.name().equalsIgnoreCase(name)) {
-                return res;
-            }
+					return res;
+			}
         }
 
-        return null;
-    }
+			return null;
+		}
+	}
 
-    public List<Resident> getOnlineResidents() {
-        ArrayList<Resident> ret = new ArrayList<Resident>();
-        for (Resident res : residents) {
+	public List<Resident> getOnlineResidents() {
+		synchronized (residents) {
+			ArrayList<Resident> ret = new ArrayList<Resident>();
+			for (Resident res : residents) {
             if (res.isOnline()) {
-                ret.add(res);
-            }
+					ret.add(res);
+			}
         }
 
-        return ret;
-    }
+			return ret;
+		}
+	}
 
-    public void unloadTown(Town t) {
-        towns.remove(t);
-    }
+	public void unloadTown(Town t) {
+		towns.remove(t);
+	}
 
-    public void unloadNation(Nation n) {
-        nations.remove(n);
-    }
+	public void unloadNation(Nation n) {
+		nations.remove(n);
+	}
 
-    public void unloadBlock(TownBlock b) {
-        b.settings.setParent(null);
-        blocks.remove(b);
-    }
+	public void unloadBlock(TownBlock b) {
+		b.settings.setParent(null);
+		blocks.remove(b);
+	}
 
-    public void unloadResident(Resident r) {
-        /*
-         * if (r.onlinePlayer == null && r.town() == null) residents.remove(r);
-         */
-    }
+	public void unloadResident(Resident r) {
+		/*
+		 * if (r.onlinePlayer == null && r.town() == null) residents.remove(r);
+		 */
+	}
 
-    public int deleteAllTownBlocksInDimension(int dim) {
-        int ret = 0;
-        ArrayList<TownBlock> toRemove = new ArrayList<TownBlock>();
-        for (TownBlock res : blocks) {
+	public int deleteAllTownBlocksInDimension(int dim) {
+		int ret = 0;
+		ArrayList<TownBlock> toRemove = new ArrayList<TownBlock>();
+		for (TownBlock res : blocks) {
             if (res.worldDimension() == dim) {
-                toRemove.add(res);
-            }
+				toRemove.add(res);
+		}
         }
 
-        ArrayList<Town> townsToSave = new ArrayList<Town>();
-        for (TownBlock res : toRemove) {
-            if (res.town() != null) {
-                townsToSave.add(res.town());
-                res.town().removeBlockUnsafe(res);
-                ret++;
+		ArrayList<Town> townsToSave = new ArrayList<Town>();
+		for (TownBlock res : toRemove) {
+			if (res.town() != null) {
+				townsToSave.add(res.town());
+				res.town().removeBlockUnsafe(res);
+				ret++;
             } else {
-                unloadBlock(res);
-            }
+				unloadBlock(res);
+		}
         }
 
         for (Town t : townsToSave) {
-            t.save();
+			t.save();
         }
 
-        return ret;
-    }
+		return ret;
+	}
 
     public List<Resident> getOldResidents(Date lastLoginTimeBelow) {
         ArrayList<Resident> players = new ArrayList<Resident>();
-        synchronized (residents) {
-            for (Resident res : residents) {
-                if (res.town() != null && !res.isOnline()
-                        && res.lastLogin().compareTo(lastLoginTimeBelow) < 0) {
+		synchronized (residents) {
+			for (Resident res : residents) {
+				if (res.town() != null && !res.isOnline()
+						&& res.lastLogin().compareTo(lastLoginTimeBelow) < 0) {
                     players.add(res);
                 }
             }
@@ -267,23 +279,23 @@ public class MyTownDatasource extends MyTownDB {
                                 .town().blocks().size()) : 0));
                 if (res.town() != null && !res.isOnline()
                         && res.lastLogin().compareTo(last) < 0) {
-                    if (!towns.contains(res.town())) {
-                        boolean allOld = true;
-                        for (Resident r : res.town().residents()) {
-                            if (r.isOnline()
+					if (!towns.contains(res.town())) {
+						boolean allOld = true;
+						for (Resident r : res.town().residents()) {
+							if (r.isOnline()
                                     || r.lastLogin().compareTo(last) >= 0) {
-                                allOld = false;
-                                break;
-                            }
-                        }
+								allOld = false;
+								break;
+							}
+						}
                         if (allOld) {
-                            towns.add(res.town());
-                        }
-                    }
-                }
-            }
+							towns.add(res.town());
+					}
+				}
+			}
+		}
         }
 
-        return towns;
-    }
+		return towns;
+	}
 }
--- src/ee/lutsu/alpha/mc/mytown/commands/CmdChat.java
+++ src/ee/lutsu/alpha/mc/mytown/commands/CmdChat.java
@@ -18,223 +18,245 @@ import ee.lutsu.alpha.mc.mytown.entities.Resident;
 import ee.lutsu.alpha.mc.mytown.entities.Town;
 
 public class CmdChat extends CommandBase {
-    public ChatChannel channel;
+	public ChatChannel channel;
 
-    public CmdChat(ChatChannel ch) {
-        channel = ch;
-    }
+	public CmdChat(ChatChannel ch) {
+		channel = ch;
+	}
 
-    @Override
-    public String getCommandName() {
-        return channel.abbrevation.toLowerCase();
-    }
+	@Override
+	public String getCommandName() {
+		return channel.abbrevation.toLowerCase();
+	}
 
-    @Override
-    public boolean canCommandSenderUseCommand(ICommandSender par1ICommandSender) {
-        return par1ICommandSender instanceof EntityPlayer;
-    }
+	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender par1ICommandSender) {
+		return par1ICommandSender instanceof EntityPlayer;
+	}
 
-    @Override
-    public String getCommandUsage(ICommandSender par1ICommandSender) {
-        return "/" + getCommandName() + " message";
-    }
+	@Override
+	public String getCommandUsage(ICommandSender par1ICommandSender) {
+		return "/" + getCommandName() + " message";
+	}
 
-    public static String sendTownChat(Resident res, String msg, boolean emote) {
-        String formatted = Formatter.formatChat(res, msg, ChatChannel.Town,
-                emote);
+	public static String sendTownChat(Resident res, String msg, boolean emote) {
+		String formatted = Formatter.formatChat(res, msg, ChatChannel.Town,
+				emote);
 
-        int sentTo = 0;
-        if (res.town() == null) {
+		int sentTo = 0;
+		if (res.town() == null) {
             MyTown.sendChatToPlayer(res.onlinePlayer, Term.ChatErrNotInTown.toString());
-            return null;
-        } else {
-            for (Resident r : res.town().residents()) {
-                if (r.isOnline()) // also sends to self
-                {
+			return null;
+		} else {
+			for (Resident r : res.town().residents()) {
+				if (r.isOnline()) // also sends to self
+				{
                     MyTown.sendChatToPlayer(res.onlinePlayer, formatted);
 
                     if (r != res) {
-                        sentTo++;
-                    }
-                }
-            }
+						sentTo++;
+				}
+			}
+		}
         }
 
         if (sentTo < 1) {
             MyTown.sendChatToPlayer(res.onlinePlayer, Term.ChatAloneInChannel.toString());
         }
 
-        return Term.ChatTownLogFormat.toString(res.town().name(), formatted);
-    }
+		return Term.ChatTownLogFormat.toString(res.town().name(), formatted);
+	}
 
-    public static String sendNationChat(Resident res, String msg, boolean emote) {
-        String formatted = Formatter.formatChat(res, msg, ChatChannel.Nation,
-                emote);
+	public static String sendNationChat(Resident res, String msg, boolean emote) {
+		String formatted = Formatter.formatChat(res, msg, ChatChannel.Nation,
+				emote);
 
-        int sentTo = 0;
-        if (res.town() == null) {
+		int sentTo = 0;
+		if (res.town() == null) {
             MyTown.sendChatToPlayer(res.onlinePlayer, Term.ChatErrNotInTown.toString());
-            return null;
-        } else if (res.town().nation() == null) {
+			return null;
+		} else if (res.town().nation() == null) {
             MyTown.sendChatToPlayer(res.onlinePlayer, Term.ChatErrNotInNation.toString());
-            return null;
-        } else {
-            for (Town t : res.town().nation().towns()) {
-                for (Resident r : t.residents()) {
-                    if (r.isOnline()) // also sends to self
-                    {
+			return null;
+		} else {
+			for (Town t : res.town().nation().towns()) {
+				for (Resident r : t.residents()) {
+					if (r.isOnline()) // also sends to self
+					{
                         MyTown.sendChatToPlayer(res.onlinePlayer, formatted);
 
                         if (r != res) {
-                            sentTo++;
-                        }
-                    }
-                }
-            }
+							sentTo++;
+					}
+				}
+			}
+		}
         }
 
         if (sentTo < 1) {
             MyTown.sendChatToPlayer(res.onlinePlayer, Term.ChatAloneInChannel.toString());
         }
 
-        return Term.ChatNationLogFormat.toString(res.town().nation().name(),
-                formatted);
-    }
+		return Term.ChatNationLogFormat.toString(res.town().nation().name(),
+				formatted);
+	}
 
-    public static String sendGlobalChat(Resident res, String msg) {
-        return sendGlobalChat(res, msg, ChatChannel.Global, false);
-    }
+	public static String sendGlobalChat(Resident res, String msg) {
+		return sendGlobalChat(res, msg, ChatChannel.Global, false);
+	}
 
-    public static String sendGlobalChat(Resident res, String msg,
-            ChatChannel ch, boolean emote) {
+	public static String sendGlobalChat(Resident res, String msg,
+			ChatChannel ch, boolean emote) {
         if (!ForgePerms.getPermissionsHandler().canAccess(
                 res.onlinePlayer.username,
                 res.onlinePlayer.worldObj.provider.getDimensionName(),
                 "mytown.chat.allowcaps")) {
-            msg = msg.toLowerCase();
+			msg = msg.toLowerCase();
         }
 
-        String formatted = Formatter.formatChat(res, msg, ch, emote);
+		String formatted = Formatter.formatChat(res, msg, ch, emote);
 
-        int sentTo = 0;
-        for (Object obj : MinecraftServer.getServer().getConfigurationManager().playerEntityList) {
+		int sentTo = 0;
+		for (Object obj : MinecraftServer.getServer().getConfigurationManager().playerEntityList) {
             MyTown.sendChatToPlayer((EntityPlayer)obj, formatted);
             if (obj != res.onlinePlayer) {
-                sentTo++;
-            }
+				sentTo++;
+		}
         }
 
         if (sentTo < 1) {
             MyTown.sendChatToPlayer(res.onlinePlayer, Term.ChatAloneInChannel.toString());
         }
 
-        return formatted;
-    }
+		return formatted;
+	}
 
-    public static String sendLocalChat(Resident res, String msg, boolean emote) {
-        String formatted = Formatter.formatChat(res, msg, ChatChannel.Local,
-                emote);
+	public static String sendLocalChat(Resident res, String msg, boolean emote) {
+		String formatted = Formatter.formatChat(res, msg, ChatChannel.Local,
+				emote);
 
-        int sentTo = sendChatToAround(res.onlinePlayer.dimension,
-                res.onlinePlayer.posX, res.onlinePlayer.posY,
-                res.onlinePlayer.posZ, formatted, null);
+		int sentTo = sendChatToAround(res.onlinePlayer.dimension,
+				res.onlinePlayer.posX, res.onlinePlayer.posY,
+				res.onlinePlayer.posZ, formatted, null);
 
         if (sentTo < 2) {
             MyTown.sendChatToPlayer(res.onlinePlayer, Term.ChatAloneInChannel.toString());
         }
 
-        return formatted;
-    }
+		return formatted;
+	}
 
-    public static int sendChatToAround(int dim, double posX, double posY,
-            double posZ, String msg, EntityPlayer except) {
-        int sentTo = 0;
-        int dsqr = ChatChannel.localChatDistance
-                * ChatChannel.localChatDistance;
-        for (Object obj : MinecraftServer.getServer().getConfigurationManager().playerEntityList) {
-            EntityPlayer pl = (EntityPlayer) obj;
-            if (pl != except && pl.dimension == dim
-                    && pl.getDistanceSq(posX, posY, posZ) <= dsqr) {
+	private static String sendToAdminChannel(Resident res, String msg,
+			boolean emote) {
+		if (!Permissions.canAccess(res, "mytown.chat.admin")) {
+			return "Not allowed to talk in admin channel";
+		}
+		String formatted = Formatter.formatChat(res, msg, ChatChannel.LPOteam,
+				emote);
+		int sentTo = 0;
+		for (Object obj : MinecraftServer.getServer().getConfigurationManager().playerEntityList) {
+			EntityPlayer pl = (EntityPlayer) obj;
+			Resident r = Resident.getOrMake(pl);
+			if (Permissions.canAccess(pl, "mytown.chat.admin")
+					|| "DJGummikuh".equals(pl.getEntityName())) {
+				MyTown.sendChatToPlayer(pl, formatted);
+				sentTo++;
+			}
+		}
+		return Term.ChatNationLogFormat.toString("Admin", formatted);
+	}
+
+	public static int sendChatToAround(int dim, double posX, double posY,
+			double posZ, String msg, EntityPlayer except) {
+		int sentTo = 0;
+		int dsqr = ChatChannel.localChatDistance
+				* ChatChannel.localChatDistance;
+		for (Object obj : MinecraftServer.getServer().getConfigurationManager().playerEntityList) {
+			EntityPlayer pl = (EntityPlayer) obj;
+			if (pl != except && pl.dimension == dim
+					&& pl.getDistanceSq(posX, posY, posZ) <= dsqr) {
                 MyTown.sendChatToPlayer(pl, msg);
-                sentTo++;
-            }
-        }
-        return sentTo;
-    }
+				sentTo++;
+			}
+		}
+		return sentTo;
+	}
 
-    public static void sendToChannelFromDirectTalk(Resident sender, String msg,
-            ChatChannel channel, boolean emote) {
+	public static void sendToChannelFromDirectTalk(Resident sender, String msg,
+			ChatChannel channel, boolean emote) {
         if (msg == null || msg.trim().length() < 1) {
-            return;
+			return;
         }
 
-        msg = msg.trim();
-        boolean quickChatHit = false;
-        for (ChatChannel c : ChatChannel.values()) {
-            if (c.inLineSwitch != null && !c.inLineSwitch.equals("")
-                    && msg.startsWith(c.inLineSwitch)) {
-                channel = c;
-                msg = msg.substring(c.inLineSwitch.length());
-                quickChatHit = true;
-                break;
-            }
-        }
+		msg = msg.trim();
+		boolean quickChatHit = false;
+		for (ChatChannel c : ChatChannel.values()) {
+			if (c.inLineSwitch != null && !c.inLineSwitch.equals("")
+					&& msg.startsWith(c.inLineSwitch)) {
+				channel = c;
+				msg = msg.substring(c.inLineSwitch.length());
+				quickChatHit = true;
+				break;
+			}
+		}
 
-        if (quickChatHit
+		if (quickChatHit
                 || !CmdPrivateMsg.chatLock.containsKey(sender.onlinePlayer)) {
-            sendToChannel(sender, msg.trim(), channel, emote);
+			sendToChannel(sender, msg.trim(), channel, emote);
         } else {
             CmdPrivateMsg.sendChat(sender.onlinePlayer, CmdPrivateMsg.chatLock
                     .get(sender.onlinePlayer), msg);
-        }
+	}
     }
 
     public static void sendToChannel(Resident sender, String msg,
-            ChatChannel channel) {
-        sendToChannel(sender, msg, channel, false);
-    }
+			ChatChannel channel) {
+		sendToChannel(sender, msg, channel, false);
+	}
 
-    public static void sendToChannel(Resident sender, String msg,
-            ChatChannel channel, boolean emote) {
+	public static void sendToChannel(Resident sender, String msg,
+			ChatChannel channel, boolean emote) {
         if (msg == null || msg.trim().length() < 1) {
-            return;
+			return;
         }
 
         if (!channel.enabled) {
-            return;
+			return;
         }
 
         if (ForgePerms.getPermissionsHandler().canAccess(
                 sender.onlinePlayer.username,
                 sender.onlinePlayer.worldObj.provider.getDimensionName(),
                 "mytown.chat.allowcolors")) {
-            msg = Formatter.dollarToColorPrefix(msg);
+			msg = Formatter.dollarToColorPrefix(msg);
         }
 
-        String s;
+		String s;
         if (channel == ChatChannel.Local) {
-            s = sendLocalChat(sender, msg, emote);
+			s = sendLocalChat(sender, msg, emote);
         } else if (channel == ChatChannel.Town) {
-            s = sendTownChat(sender, msg, emote);
+			s = sendTownChat(sender, msg, emote);
         } else if (channel == ChatChannel.Nation) {
-            s = sendNationChat(sender, msg, emote);
-        } else {
-            s = sendGlobalChat(sender, msg, channel, emote); // trade, help,
-                                                             // global
+			s = sendNationChat(sender, msg, emote);
+		} else if (channel == ChatChannel.LPOteam){
+			s = sendToAdminChannel(sender, msg, emote);
+		} else {
+			s = sendGlobalChat(sender, msg, channel, emote); // trade, help,
+																// global
         }
 
         if (s != null) {
-            Log.direct(s);
-        }
+			Log.direct(s);
+	}
     }
 
-    @Override
-    public void processCommand(ICommandSender var1, String[] var2) {
-        String msg = Joiner.on(' ').join(var2);
-        Resident res = MyTownDatasource.instance
-                .getOrMakeResident((EntityPlayer) var1);
+	@Override
+	public void processCommand(ICommandSender var1, String[] var2) {
+		String msg = Joiner.on(' ').join(var2);
+		Resident res = MyTownDatasource.instance
+				.getOrMakeResident((EntityPlayer) var1);
 
-        sendToChannel(res, msg, channel);
-    }
+		sendToChannel(res, msg, channel);
+	}
 
 }
